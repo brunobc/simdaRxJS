4026e98ffe4044b6ec97efc8e5cb7202fcc1a0e7
brunobc <brunobezerrachaves@gmail.com>
2017-05-21 23:36:17 -0300

    Base 64

--- public/app/core/loader.ts
+++ public/app/core/loader.ts
@@ -28,29 +28,6 @@ export function load(url: string) {
     }).retryWhen(retryStrategy({ attempts: 3, delay: 3500 }));
 }
 
-var myHeaders = new Headers();
-
-var init = {
-    method: 'POST',
-    headers: myHeaders,
-    mode: 'cors',
-    cache: 'default'
-};
-
-export function loadWithFetch(url: string) {
-    return Observable.defer(() => {
-        return Observable.fromPromise(
-            fetch(url, init).then(r => {
-                if (r.status === 200) {
-                    return r.json();
-                } else {
-                    return Promise.reject(r);
-                }
-            }))
-    }).retryWhen(retryStrategy());
-
-}
-
 function retryStrategy({ attempts = 4, delay = 1000 } = {}) {
     return function (errors) {
         return errors
@@ -66,3 +43,95 @@ function retryStrategy({ attempts = 4, delay = 1000 } = {}) {
             .delay(delay);
     }
 }
+
+// var lat = value.geometry.coordinates[0].substring(0, 16);
+// var lon = value.geometry.coordinates[1].substring(1, 17);
+// value.geometry.coordinates[0] = parseFloat((Base64.decode(lat)));
+// value.geometry.coordinates[1] = parseFloat((Base64.decode(lon)));
+var Base64 = {
+    _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+    encode: function (e) {
+        var t = "";
+        var n, r, i, s, o, u, a;
+        var f = 0;
+        e = Base64._utf8_encode(e);
+        while (f < e.length) {
+            n = e.charCodeAt(f++);
+            r = e.charCodeAt(f++);
+            i = e.charCodeAt(f++);
+            s = n >> 2;
+            o = (n & 3) << 4 | r >> 4;
+            u = (r & 15) << 2 | i >> 6;
+            a = i & 63;
+            if (isNaN(r)) {
+                u = a = 64
+            } else if (isNaN(i)) {
+                a = 64
+            }
+            t = t + this._keyStr.charAt(s) + this._keyStr.charAt(o) + this._keyStr.charAt(u) + this._keyStr.charAt(a)
+        } return t
+    },
+    decode: function (e) {
+        var t = "";
+        var n, r, i;
+        var s, o, u, a;
+        var f = 0;
+        e = e.replace(/[^A-Za-z0-9+/=]/g, "");
+        while (f < e.length) {
+            s = this._keyStr.indexOf(e.charAt(f++));
+            o = this._keyStr.indexOf(e.charAt(f++));
+            u = this._keyStr.indexOf(e.charAt(f++));
+            a = this._keyStr.indexOf(e.charAt(f++));
+            n = s << 2 | o >> 4; r = (o & 15) << 4 | u >> 2;
+            i = (u & 3) << 6 | a; t = t + String.fromCharCode(n);
+            if (u != 64) {
+                t = t + String.fromCharCode(r)
+            }
+            if (a != 64) {
+                t = t + String.fromCharCode(i)
+            }
+        }
+        t = Base64._utf8_decode(t);
+        return t;
+    },
+    _utf8_encode: function (e) {
+        e = e.replace(/rn/g, "n");
+        var t = "";
+        for (var n = 0; n < e.length; n++) {
+            var r = e.charCodeAt(n);
+            if (r < 128) {
+                t += String.fromCharCode(r)
+            } else if (r > 127 && r < 2048) {
+                t += String.fromCharCode(r >> 6 | 192);
+                t += String.fromCharCode(r & 63 | 128)
+            } else {
+                t += String.fromCharCode(r >> 12 | 224);
+                t += String.fromCharCode(r >> 6 & 63 | 128);
+                t += String.fromCharCode(r & 63 | 128)
+            }
+
+        }
+        return t;
+    }, _utf8_decode: function (e) {
+        var t = "";
+        var n = 0;
+        var c1, c2, c3;
+        var r = c1 = c2 = 0;
+        while (n < e.length) {
+            r = e.charCodeAt(n);
+            if (r < 128) {
+                t += String.fromCharCode(r); n++
+            } else if (r > 191 && r < 224) {
+                c2 = e.charCodeAt(n + 1);
+                t += String.fromCharCode((r & 31) << 6 | c2 & 63);
+                n += 2
+            } else {
+                c2 = e.charCodeAt(n + 1);
+                c3 = e.charCodeAt(n + 2);
+                t += String.fromCharCode((r & 15) << 12 | (c2 & 63) << 6 | c3 & 63);
+                n += 3
+            }
+        }
+        return t
+    }
+};
